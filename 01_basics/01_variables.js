b6975649fd6255af5bfee0140c93654f2672fce7
Chetan B L <145449992+Chetanbl2003@users.noreply.github.com>
2025-03-24 18:16:28 +0000

    added variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123214
+let accountEmail = "chetan@outlook.com"
+var accountPassword = "125234" // using the "var" is not a good practise because it leads to get error. So don't use "var"
+accountCity = "Chennai"
+let accountState
+
+accountEmail = "chetan@Microsoft.com"
+accountPassword = "54682"
+accountCity = "Bangalore"
+
+console.table([ accountId, accountEmail, accountPassword, accountCity, accountState])
