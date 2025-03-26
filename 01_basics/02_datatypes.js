35473eafdaaeaefd8e5a5ffb248493b43b9da67d
Chetan B L <145449992+Chetanbl2003@users.noreply.github.com>
2025-03-26 17:48:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //after using this line the machine will treat all the lines of code as newer version of javascript.
+
+// alert(3+3) //to run this line we are going to use node.js not browser
+
+/* number = 2 to the power 53 (range) 
+bigInt for larger data usually we are normally going to use int
+string = ""
+boolean = true/false
+undefined = (eg : let name; (without initialising any datatype when We use "typeof" then it will return as "undefined" )) 
+symbol = unique 
+null = standalone value (when used "typeof" then it will return the output as "object") */
+
+console.log(typeof undefined); // output : undefined
+console.log(typeof null); // output : object
