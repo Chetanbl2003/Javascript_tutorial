cccfb0ea485c4b825ff1414391dfe8871edbe5c6
Chetan B L <145449992+Chetanbl2003@users.noreply.github.com>
2025-04-02 07:10:53 +0000

    added some more files

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+console.log(null >= 0) // output : true
+console.log(undefined == 0) // output ; false
+
+// strict  comparison "==="
+
+console.log("2" === 2) //output : false
---
+++ datatypesSummary.js
@@ -0,0 +1,20 @@
+// Based on how the data is been stored in the memory  and how the data is been accessed from the memory.
+// The datatype is been categorized as , namely :
+// 1. Primitive datatype (Call-by-value) : 
+//      7 types - String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+// 2. Non-primitive datatype (Call-by-reference) :
+//      Array, objects and Functions
+
+// Declaration of Array, objects and Functions
+
+let myArray = ["Chetan", 19, 2003 ]
+
+const myObj = {
+    name : Chetan,
+    age : 22,
+}
+
+const myFunction = function () {
+    console.log("Hello world");
+}
